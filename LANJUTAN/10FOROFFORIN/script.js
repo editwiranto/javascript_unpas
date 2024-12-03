835147f8853e245ab1bb8259e35579e638d8f2a4
editwiranto497 <horasjaya86@gmail.com>
2024-12-03 15:34:12 +0700

    tentang for of

---
+++ LANJUTAN/10FOROFFORIN/script.js
@@ -0,0 +1,52 @@
+const mhs = ['Edit Wiranto', 'Rizki', 'Steven'];
+
+// for (let i = 0; i < mhs.length; i++) {
+//     console.log(mhs[i]);
+// }
+
+// mhs.forEach(m => console.log(m));
+
+const edit = 'Edit wiranto';
+
+// for (const m of mhs) {
+//     console.log(m);
+// }
+
+
+// for (const m of edit) {
+//     console.log(m);
+// }
+
+// mhs.forEach((m, i) => {
+//     console.log(`${m} adalah Mahasiswa ke-${i}`);
+// })
+
+// for (const [i, m] of mhs.entries()){
+//     console.log(`${m} adalah Mahasiswa ke-${i}`);
+// }
+
+//nodelist
+
+const li = document.querySelectorAll('.nama');
+
+// li.forEach(m => console.log(m.textContent));
+
+// for (const i of li) {
+//     console.log(i.innerHTML);
+// }
+
+// arguments
+
+function jumlahAngka() {
+
+    jumlah = 0;
+    // arguments.forEach(m => jumlah += m);
+    for (const i of arguments) {
+        jumlah += i;
+    }
+    return jumlah;
+}
+
+console.log(jumlahAngka(1, 2, 3, 4, 5));
+
+
